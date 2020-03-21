103d70107a9ec6b9a18dce9b3655620cf0764f30
Brian Postlethwaite <brian.postlethwaite@health.telstra.com>
2020-03-21 12:34:12 +1100

    NotifyPropertyChanged events

--- ClinicArrivals.Models/DoctorRoomLabelMappings.cs
+++ ClinicArrivals.Models/DoctorRoomLabelMappings.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace ClinicArrivals.Models
 {
+    [AddINotifyPropertyChangedInterface]
     public class DoctorRoomLabelMappings
     {
         /// <summary>
