defa8fe414b89e7a84c8ee60e9851a33f4293100
Brian Postlethwaite <brian.postlethwaite@health.telstra.com>
2020-03-23 03:29:16 +1100

    Stub Video Conference Manager Class

---
+++ ClinicArrivals.Models/VideoConferenceManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicArrivals.Models
+{
+    public class VideoConferenceManager : IVideoConferenceManager
+    {
+        public void Initialize(Settings settings)
+        {
+
+        }
+
+        /// <summary>
+        /// Get URL for conference
+        /// </summary>
+        /// <param name="id">The id of the appointment (unique ==> Appointment Resource id)</param>
+        public String getConferenceUrl(String appointmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Return true if it's possible to know if the patient has joined (not always possible with video services)
+        /// </summary>
+        /// <param name="id">The id of the appointment (unique ==> Appointment Resource id)</param>
+        public Boolean canKnowIfJoined()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Return true if someone (assumed to be the patient) has joined the conference call
+        /// </summary>
+        /// <param name="id">The id of the appointment (unique ==> Appointment Resource id)</param>
+        public Boolean hasSomeoneJoined(String appointmentId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
